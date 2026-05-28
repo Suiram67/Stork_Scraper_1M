8473cbe0955bcba7fcec90e12d44217dd5e18544
Arnold <mariusarnoldpro@gmail.com>
2026-05-28 14:50:55 +0200

    chgt date

--- Scraper_Stork_1.py
+++ Scraper_Stork_1.py
@@ -2,12 +2,12 @@ import requests
 import json
 import re
 from bs4 import BeautifulSoup
-from datetime import datetime 
+from datetime import datetime, timedelta
 URL = "https://www.cigogne-management.com/fr/bank/cm/-/sie-98-xs3184046822"
 reponse = requests.get(URL)
 
 idx = reponse.text.find("100578")
-#print(reponse.text[idx-300:idx+200])
+print(reponse.text[idx-300:idx+200])
 
 matche = re.search(r"data:(\[\[.*?\]\])",reponse.text)
 data = json.loads(matche.group(1))
@@ -27,7 +27,7 @@ dico={}
 with open("Scraper_stork.csv","w",encoding = "utf-8") as l:
     for key,val in data:
     
-        key_dico = datetime.fromtimestamp(key/1000)
+        key_dico = datetime.fromtimestamp(key/1000)+timedelta(hours=2)
         dico[key_dico] = val
         ligne = f"{key_dico};{str(val)}\n"
         l.write(ligne)
